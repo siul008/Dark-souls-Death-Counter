c2672d1945bf57c816f0abc907acbc74de48703b
siul008 <Julien.nunespro@gmail.com>
2022-03-05 18:36:05 +0100

    .

--- obj/Debug/net5.0-windows/Dark souls Death Counter.AssemblyInfo.cs
+++ obj/Debug/net5.0-windows/Dark souls Death Counter.AssemblyInfo.cs
@@ -11,13 +11,14 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Dark souls Death Counter")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Elden Ring Death Counter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Dark souls Death Counter")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Dark souls Death Counter")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyCopyrightAttribute("Siul008")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.1")]
+[assembly: System.Reflection.AssemblyProductAttribute("Elden Ring Death Counter")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Elden Ring Death Counter")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.1.0")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
 
